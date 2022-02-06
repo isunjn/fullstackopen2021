fd8a2b42660e0247ed283b06699760729e9fe11c
Jianan Sun <isunjn@gmail.com>
2022-02-06 12:38:23 +0800

    3a

---
+++ part3/phonebookbackend/index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(express.json());
+
+morgan.token('body', (req, res) => {return JSON.stringify(req.body);});
+
+app.use(morgan((tokens, req, res) => {
+  if (tokens.method(req, res) === 'POST') {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens['body'](req, res)
+    ].join(' ')  
+  } else {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+    ].join(' ')  
+  }
+}));
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} peopele</p><p>${new Date()}</p>`);
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+const MAX_ID = 1000;
+const MIN_ID = 1;
+const generateId = () => {
+  return Math.floor(Math.random() * (MAX_ID - MIN_ID + 1) + MIN_ID);
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'content missing'
+    });
+  }
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing'
+    });
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
