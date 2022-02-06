e6933be253977792a047da60251a98ab56d268fc
Jianan Sun <isunjn@gmail.com>
2022-02-06 14:49:14 +0800

    3b

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.use(express.static('build'));
+
 app.use(express.json());
 
 morgan.token('body', (req, res) => {return JSON.stringify(req.body);});
@@ -119,7 +121,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
