d3c17aba2a993d0aaabdadad981a935c0275f6c5
Jianan Sun <isunjn@gmail.com>
2022-02-12 15:12:23 +0800

    6a

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { createNew } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const create = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    dispatch(createNew(content))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={create}>
+        <div><input name='anecdote' /></div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  const stats = store.getState()
+
+  return (
+    <div>
+      <button onClick={good}>good</button> 
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
+      <div>good {stats.good}</div>
+      <div>ok {stats.ok}</div>
+      <div>bad {stats.bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
